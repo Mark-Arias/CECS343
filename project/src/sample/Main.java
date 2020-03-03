9c2c4ca121f352fae7d5d1c282f3e1452ad51fba
Steven <stevenmatthew.arce@gmail.com>
2020-03-02 22:24:56 -0800

    Updated Main

--- project/src/sample/Main.java
+++ project/src/sample/Main.java
@@ -1,21 +1,27 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void start(Stage stage) {
+        Text text = new Text();
+        text.setText("Team Mamba");
 
+        text.setX(50);
+        text.setY(50);
+
+        Group root = new Group(text);
+
+        Scene scene = new Scene(root, 500, 500);
+        stage.setTitle("ATM");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
