90ea6231b78fdf7f3d297975088265ee29d374ed
smcarce <stevenarce650@gmail.com>
2020-04-22 18:52:39 -0700

    Update ATM.java

--- project/src/Backend/ATM.java
+++ project/src/Backend/ATM.java
@@ -1,4 +1,38 @@
 package Backend;
 
 public class ATM {
+    private String bankName, state;
+    private long ATM_id;
+    private Account account;
+
+    public ATM(String bankName, long ATM_id, Account account, String state) {
+        this.bankName = bankName;
+        this.ATM_id = ATM_id;
+        this.account = account;
+        this.state = state;
+    }
+
+    String getBankName() {
+        return bankName;
+    }
+
+    long getATM_id() {
+        return ATM_id;
+    }
+
+    Account getAccount() {
+        return account;
+    }
+
+    String getState() {
+        return state;
+    }
+
+    void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    void setATM_id(long ATM_id) {
+        this.ATM_id = ATM_id;
+    }
 }
