2123ec00f7aea599f017e1f18df9fcf6f5db714b
smcarce <stevenarce650@gmail.com>
2020-04-22 19:21:12 -0700

    Added constructor for ATM_Card.java

--- project/src/Backend/ATM_Card.java
+++ project/src/Backend/ATM_Card.java
@@ -4,5 +4,8 @@ public class ATM_Card {
     private String cardNumber;
     private short PIN;
 
-
+    public ATM_Card(String cardNumber, short PIN) {
+        this.cardNumber = cardNumber;
+        this.PIN = PIN;
+    }
 }
